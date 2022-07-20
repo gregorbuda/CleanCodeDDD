9658698d979e4b9dcd724a8751101ab7482bc967
Gregor Duarte Martinez <gduarte@xirectds.com>
2022-07-20 15:22:58 -0400

    models

--- API/Compliance.Api/Program.cs
+++ API/Compliance.Api/Program.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.OpenApi.Models;
-using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
